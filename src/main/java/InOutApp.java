5b591ecf91be7d5fdf2366ae022aa147911792bf
Emil Piękoś <piekosemil@gmail.com>
2024-06-10 13:12:09 +0200

    Second commit.

---
+++ src/main/java/InOutApp.java
@@ -0,0 +1,40 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class InOutApp {
+    static FileWriter fileWriter;
+    static BufferedWriter writer;
+
+    protected static double getResult(String operator, double number1, double number2) {
+        double result;
+        result = switch (operator) {
+            case "+" -> number1 + number2;
+            case "-" -> number1 - number2;
+            case "*" -> number1 * number2;
+            case "/" -> number1 / number2;
+            default -> 0;
+        };
+        return result;
+    }
+
+    protected static void writeResult(File file, double number1, String operator, double number2, double result) {
+        try {
+            fileWriter = new FileWriter(file, false);
+            writer = new BufferedWriter(fileWriter);
+            writer.write(number1 + " " + operator + " " + number2 + " = " + result);
+            writer.write(System.lineSeparator());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected static void writerCloser() throws IOException {
+        writer.close();
+    }
+
+    protected static void printResult(double number1, String operator, double number2, double result) {
+        System.out.println(number1 + " " + operator + " " + number2 + " = " + result);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,30 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        String fileName = "operations.txt";
-        File file = new File(fileName);
-        Scanner scan = new Scanner(file);
+    public static void main(String[] args) throws IOException {
+        String fileInput = "operations.txt";
+        String fileOutput = "results.txt";
+        File inputFile = new File(fileInput);
+        File outputFile = new File(fileOutput);
+        Scanner scan = new Scanner(inputFile);
+        double number1 = 0;
+        String operator = null;
+        double number2 = 0;
+        double result = 0;
 
         while (scan.hasNextDouble() | scan.hasNextLine()) {
-            double number1 = scan.nextInt();
-            String operator = scan.next();
-            double number2 = scan.nextInt();
-            double result = 0;
-            if (operator.equals("+")) {
-                result = number1 + number2;
-            } else if (operator.equals("-")) {
-                result = number1 - number2;
-            } else if (operator.equals("*")) {
-                result = number1 * number2;
-            } else if (operator.equals("/")) {
-                result = number1 / number2;
-            }
-            System.out.println(number1 + " " + operator + " " + number2 + " = " + result);
+            number1 = scan.nextDouble();
+            operator = scan.next();
+            number2 = scan.nextDouble();
+            result = InOutApp.getResult(operator, number1, number2);
+            InOutApp.printResult(number1, operator, number2, result);
+            InOutApp.writeResult(outputFile, number1, operator, number2, result);
         }
+        InOutApp.writerCloser();
         scan.close();
     }
 }
